d8abd4e80ccb6740a3d61c88c59da8a5d718904e
maurofv00 <mauroferrarirc@gmail.com>
2025-08-18 20:59:43 -0300

    Primer test: Implementacion funcion

---
+++ kata.py
@@ -0,0 +1,5 @@
+#Suma los números en una string separados por comas.
+#Una string vacía devuelve 0.
+def suma(cadena):
+  if cadena == "":
+        return 0
--- test_kata.py
+++ test_kata.py
@@ -1,5 +1,5 @@
 import pytest
-from kata import suma   # Importamos la función desde suma.py
+from kata import suma   # Importamos la función desde kata.py
 
 def test_suma_cadena_vacia():
     assert suma("") == 0

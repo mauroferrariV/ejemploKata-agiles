47475445d0bc0eacad722792feb229efd02ecc23
maurofv00 <mauroferrarirc@gmail.com>
2025-08-18 21:24:47 -0300

    Quinto test: implementacion

--- kata.py
+++ kata.py
@@ -1,8 +1,15 @@
 #Suma los números en una string separados por comas.
 #Una string vacía devuelve 0.
+#
 def suma(cadena):
   if cadena == "":
         return 0
-  numeros = cadena.split(",")
-  numeros = [int(n) for n in numeros]
-  return sum(numeros)
+  caracteres = cadena.split(",")
+  for c in caracteres :
+    if not c.strip().isdigit():  # validación de cada número
+      raise ValueError(f"Valor inválido: '{c}' (solo números permitidos)")
+  return sum(int(c) for c in caracteres)
+
+res = suma('1,a,3')
+print(res)
+
